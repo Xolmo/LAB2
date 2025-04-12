ead6dd28ac595104b14f1cf6a81fcd4dbce13b84
Xolmo <a20220378@pucp.edu.pe>
2025-04-11 21:39:18 -0500

    Se hizo lo que se pudo

--- src/main/java/com/example/lab2/Controller/AutoController.java
+++ src/main/java/com/example/lab2/Controller/AutoController.java
@@ -1,9 +1,11 @@
 package com.example.lab2.Controller;
 
+import com.example.lab2.Entity.Auto;
 import com.example.lab2.Repository.AutoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +19,16 @@ public class AutoController {
     public String listaAutos(Model model) {
         model.addAttribute("listaAutos", autoRepository.findAll());
         return "auto/lista";
+    }
+
+    @GetMapping("/new")
+    public String newAutoForm(Model model) {
+        return "auto/form";
+    }
+
+    @PostMapping("/save")
+    public String saveAuto(Auto auto, Model model) {
+        autoRepository.save(auto);
+        return "redirect:/auto";
     }
 }
--- src/main/java/com/example/lab2/Controller/SedeController.java
+++ src/main/java/com/example/lab2/Controller/SedeController.java
@@ -1,9 +1,12 @@
 package com.example.lab2.Controller;
 
+import com.example.lab2.Entity.Auto;
+import com.example.lab2.Entity.Sede;
 import com.example.lab2.Repository.SedeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +20,16 @@ public class SedeController {
     public String listaSedes(Model model) {
         model.addAttribute("listaSedes", sedeRepository.findAll());
         return "sede/lista";
+    }
+
+    @GetMapping("/new")
+    public String newSedeForm(Model model) {
+        return "sede/form";
+    }
+
+    @PostMapping("/save")
+    public String saveSede(Sede sede, Model model) {
+        sedeRepository.save(sede);
+        return "redirect:/sede";
     }
 }
